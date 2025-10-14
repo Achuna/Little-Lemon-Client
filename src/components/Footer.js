75fafafaead64f6ee21c4fe017ff5bd80e9c896c
Achuna <achunaofonedu@gmail.com>
2025-10-13 21:02:17 -0400

    Semantic structure of project

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Little Lemon Restaurant. All Rights Reserved.</p>
+      <p>
+        <a href="mailto:info@littlelemon.com">info@littlelemon.com</a> | 
+        <a href="tel:+11234567890"> (123) 456-7890</a>
+      </p>
+      <p>123 Main Street, Chicago, IL</p>
+    </footer>
+  );
+}
